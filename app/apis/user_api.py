3bc56a25cc363ff462dac70004769b6f38eedee2
vincent94hong <vincent94hong@gmail.com>
2023-03-16 12:58:05 +0900

    user_api refactoring

--- app/apis/user_api.py
+++ app/apis/user_api.py
@@ -22,15 +22,16 @@ user = ns.model('User', {
     'created_at': fields.DateTime(description='생성일'),
 })
 
+id_parser = reqparse.RequestParser()
+id_parser.add_argument('id', required=True, help='user id')
 
-parser = reqparse.RequestParser()
-parser.add_argument('id', required=True, help='user id')
+parser = id_parser.copy()
 parser.add_argument('pw', required=True, help='user pw')
 parser.add_argument('name', required=True, help='user name')
 parser.add_argument('email', required=False, help='user email')
 parser.add_argument('phone', required=False, help='user phone')
 
-put_parser = reqparse.RequestParser()
+put_parser = id_parser.copy()
 put_parser.add_argument('pw', required=False, help='user pw')
 put_parser.add_argument('name', required=False, help='user name')
 put_parser.add_argument('email', required=False, help='user email')
@@ -41,20 +42,28 @@ login_parser.add_argument('id', required=True, help='user id')
 login_parser.add_argument('pw', required=True, help='user pw')
 
 
+def admin_check():
+    if g.user.id != 'admin':
+        return abort(403)
+    
+
 @ns.route('')
 class UserList(Resource):
-
+    @ns.expect(id_parser)
     @ns.marshal_list_with(user, skip_none=True)
     def get(self):
-        '''관리자 유저 복수 조회'''
-        if g.user.id != 'admin':
-            return abort(403)
+        '''관리자 유저 조회'''
+        admin_check()
+        id = id_parser.parse_args()['id']
+        if id:
+            return UserModel.find_one_by_user_id(id)
         return UserModel.query.all()
-    
+
     @ns.expect(parser)
     @ns.marshal_list_with(user, skip_none=True)
     def post(self):
         '''유저 생성'''
+        admin_check()
         args = parser.parse_args()
         id = args['id']
         user = UserModel.find_one_by_user_id(id)
@@ -70,28 +79,15 @@ class UserList(Resource):
         g.db.add(user)
         g.db.commit()
         return user, 201
-
-
-@ns.route('/admin/<id>')
-@ns.param('id', '유저 아이디')
-class UserAdmin(Resource):
-
-    @ns.marshal_list_with(user, skip_none=True)
-    def get(self, id):
-        '''관리자 유저 단수 조회'''
-        if g.user.id != 'admin':
-            return abort(403)
-        user = UserModel.find_one_by_user_id(id)
-        return user
     
     @ns.expect(put_parser)
     @ns.marshal_list_with(user, skip_none=True)
-    def put(self, id):
+    def put(self):
         '''관리자 유저 수정'''
+        admin_check()
         args = put_parser.parse_args()
+        id = args['id']
         user = UserModel.find_one_by_user_id(id)
-        if g.user.id != 'admin':
-            ns.abort(403)
         if args['pw'] is not None:
             user.pw = security.generate_password_hash(args['pw'])
         if args['name'] is not None:
@@ -119,8 +115,6 @@ class User(Resource):
         '''마이페이지 수정'''
         args = put_parser.parse_args()
         user = UserModel.find_one_by_user_id(g.user.id)
-        if user.id != g.user.id:
-            ns.abort(403)
         if args['pw'] is not None:
             user.pw = security.generate_password_hash(args['pw'])
         if args['name'] is not None:
@@ -136,13 +130,14 @@ class User(Resource):
     def delete(self):
         '''마이페이지 삭제'''
         user = UserModel.find_one_by_user_id(g.user.id)
-        session.pop('user_id', None)
-        g.user = None
         g.db.delete(user)
         g.db.commit()
+        session.pop('user_id', None)
+        g.user = None
         return '', 204
     
 
+# 추후 없어질 기능. 지금은 테스트 용으로 사용
 @ns.route('/login')
 class UserLogin(Resource):
 
